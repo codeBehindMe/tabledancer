93cb274423fa677a0471ab7c4b7a269e71b85345
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-07-29 04:19:46 +0000

    Added exclusion of test module

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+from tabledancer.models.lifecycle_policy import ErrorOnSchemaChange
 from setuptools import find_packages, setup
 
 setup(
     name="tabledancer",
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     entry_points={"console_scripts": ["tabledancer=tabledancer.main:hello"]},
 )
