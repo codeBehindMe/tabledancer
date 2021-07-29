a1dfcbcb89f3dc9e4c125dd05525c57fbf456cb5
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-07-29 04:37:00 +0000

    Excluded test from build

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 
 setup(
     name="tabledancer",
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(exclude=["test"]),
     entry_points={"console_scripts": ["tabledancer=tabledancer.main:hello"]},
 )
