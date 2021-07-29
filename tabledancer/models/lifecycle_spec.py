ef01aaaeb25881233769cc635381172bb4dcf9dc
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-07-29 10:57:01 +0000

    AIT-198: Moved name out of base to table spec

--- tabledancer/models/lifecycle_spec.py
+++ tabledancer/models/lifecycle_spec.py
@@ -4,14 +4,12 @@ from typing import Any, Dict
 class LifeCycleSpec:
     def __init__(
         self,
-        name: str,
         backend: str,
         database_name: str,
         life_cycle_policy: Dict[str, Any],
         table_spec: Dict[str, Any],
     ) -> None:
 
-        self.name = name
         self.backend = backend
         self.database_name = database_name
         self.life_cycle_policy = life_cycle_policy
