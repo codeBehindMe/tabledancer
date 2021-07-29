c97511d4e7258e9d4bb7c0d9a688ff7cc47f70e7
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-07-29 11:23:46 +0000

    AIT-198: Added parser

---
+++ tabledancer/parsers/parser.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict
+
+from tabledancer.dancers.dancer import IDancer
+from tabledancer.models.lifecycle_policy import (LifeCyclePolicy,
+                                                 life_cycle_policies)
+from tabledancer.models.lifecycle_spec import LifeCycleSpec
+from tabledancer.models.table_spec import TableSpec
+
+
+class Parser:
+    def __init__(self, dancer: IDancer) -> None:
+        self.dancer = dancer
+
+    def parse_life_cycle_spec_dict(
+        self, life_cycle_spec_dict: Dict[str, Any]
+    ) -> LifeCycleSpec:
+
+        life_cycle_spec_dict["life_cycle_policy"] = self._parse_life_cycle_policy_dict(
+            life_cycle_spec_dict["life_cycle_policy"]
+        )
+        life_cycle_spec_dict["table_spec"] = self._parse_table_spec_dict(
+            life_cycle_spec_dict["table_spec"], self.dancer
+        )
+        return LifeCycleSpec(**life_cycle_spec_dict)
+
+    def _parse_life_cycle_policy_dict(
+        self, life_cycle_policy_dict: Dict[str, Any]
+    ) -> LifeCyclePolicy:
+
+        # FIXME [Aaron]: Strings in code which are reused in YAML files and
+        #   possibly other locations in the future.
+        policy_name = life_cycle_policy_dict["policy"]
+        policy_properties = life_cycle_policy_dict["properties"]
+
+        if policy_properties is not None:
+            return life_cycle_policies[policy_name](**policy_properties)
+        return life_cycle_policies[policy_name]()
+
+    def _parse_table_spec_dict(
+        self, table_spec_dict: Dict[str, Any], dancer: IDancer
+    ) -> TableSpec:
+
+        return dancer.parse_table_spec(table_spec_dict)
