5389b68986851dd5fee20e3c91b67c2280527ac2
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-07-29 08:31:10 +0000

    AIT-198: Added utils module

---
+++ tabledancer/utils/misc.py
@@ -0,0 +1,21 @@
+from os import path, truncate
+
+import yaml
+
+
+def read_yaml(path_to_yaml_file: str, allow_unsafe: bool = False):
+    """Loads a yaml file to a dictionary from disk.
+
+    Args:
+        path_to_yaml_file (str): Path to the yaml file.
+        allow_unsafe (bool, optional): Unsafe mode allows to load complex yaml
+          tags. Defaults to False.
+
+    Returns:
+        [type]: [description]
+    """
+
+    with open(path_to_yaml_file, "r") as f:
+        if allow_unsafe is True:
+            return yaml.load(f)
+        return yaml.safe_load(f)
---
+++ test/test_tabledancer/test_utils/test_misc.py
@@ -0,0 +1,22 @@
+from typing import Dict
+
+import pytest
+
+from tabledancer.utils.misc import read_yaml
+
+
+@pytest.fixture(scope="class")
+def test_yaml_file_path() -> str:
+    return "test/resources/simple_table.yaml"
+
+
+@pytest.mark.usefixtures("test_yaml_file_path")
+class TestReadYaml:
+    def test_read_yaml_returns_dict(self, test_yaml_file_path: str):
+        """Check's that the read_yaml function returns a dict.
+
+        Args:
+            test_yaml_file_path (str): Path to the test yaml file.
+        """
+
+        assert isinstance(read_yaml(test_yaml_file_path), Dict)
