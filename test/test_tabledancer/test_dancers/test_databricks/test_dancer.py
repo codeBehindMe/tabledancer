f313dfc97854c85f26ae7e3079f5c68585267968
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-07-30 08:50:01 +0000

    AIT-198: Changed sample table reference in db test

--- test/test_tabledancer/test_dancers/test_databricks/test_dancer.py
+++ test/test_tabledancer/test_dancers/test_databricks/test_dancer.py
@@ -9,7 +9,7 @@ from tabledancer.utils.misc import read_yaml_file
 
 @pytest.fixture(scope="class")
 def db_table_spec_dict() -> Dict[str, Any]:
-    return read_yaml_file("test/resources/simple_table.yaml")["table_spec"]
+    return read_yaml_file("test/resources/basic_db_table.yaml")["table_spec"]
 
 
 @pytest.mark.usefixtures("db_table_spec_dict")
