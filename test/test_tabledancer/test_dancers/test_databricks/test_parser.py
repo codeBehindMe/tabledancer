45b2ebad0ecfe0fcffe8ac1e91fed11ac3c5842e
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-08-03 09:57:29 +0000

    AIT-198: Added databricks parser

---
+++ test/test_tabledancer/test_dancers/test_databricks/test_parser.py
@@ -0,0 +1,18 @@
+
+import pytest
+from tabledancer.dancers.databricks.parser import DatabricksDDLParser
+
+@pytest.fixture(scope="class")
+def simple_ddl() -> str:
+  return "CREATE TABLE `gth_prediction`.`predictions` (\n  `policy_id` STRING,\n  `probability` DOUBLE,\n  `prediction` INT,\n  `end_of_month` DATE,\n  `run_id` BIGINT)\nUSING delta\nOPTIONS (\n  `overwriteSchema` 'true')\nTBLPROPERTIES (\n  'overwriteSchema' = 'true')\n"
+
+
+@pytest.mark.usefixtures('simple_ddl')
+class TestDatabricksDDLParser:
+
+  def test_extract_database_name_gets_correct_db_name(self, simple_ddl:str):
+    ddl_parser = DatabricksDDLParser(simple_ddl)
+
+    pass
+    # assert ddl_parser._get_database_name() == "gth_prediction"
+
