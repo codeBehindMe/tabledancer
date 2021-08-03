e083d9b6d9efa7eb34c9e013d79bfa0859177249
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-08-03 05:31:49 +0000

    AIT-198: Added databricks backend functions

--- tabledancer/dancers/databricks/dancer.py
+++ tabledancer/dancers/databricks/dancer.py
@@ -22,5 +22,20 @@ class DatabricksDancer(IDancer):
         # FIXME: Docstring
         return DatabricksTableSpec(**table_spec_dict)
 
+    # FIXME: Make abstract and enforce in IDancer interface.
+    def _check_if_table_exists(self, table_spec : DatabricksTableSpec) -> bool:
+        # FIXME: Docstring
+        return self.spark._jsparkSession.catalog().tableExists(table_spec.database, table_spec.name)
+
+    # FIXME: Make abstract and enforce in IDancer interface.
+    def _get_table_ddl_in_backend(self, table_spec: DatabricksTableSpec) -> str:
+        # FIXME: Docstring
+        return self.spark.sql(f"SHOW CREATE TABLE {table_spec.database}.{table_spec.name}").collect()[0][0]
+
     def get_table_ddl_from_backend(self, table_spec: DatabricksTableSpec) -> str:
-        pass
+        # FIXME: Docstring
+
+        if self._check_if_table_exists(table_spec) is not True:
+            raise NotImplemented("Create table handler not implmented")
+
+        ddl = self._get_table_ddl_in_backend(table_spec)
