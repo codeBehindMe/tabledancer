3ed656be74bb7312aaa5cf8709f767ba3f8a1753
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-08-17 08:59:44 +0000

    added from ddl info method

--- tabledancer/dancers/deltabricks/dancer.py
+++ tabledancer/dancers/deltabricks/dancer.py
@@ -1,6 +1,7 @@
 
 from __future__ import annotations
-from typing import Any, Dict, Optional
+from tabledancer.utils.misc import is_none_or_empty_string
+from typing import Any, Dict, Generator, List, Optional, Tuple
 from pyspark.sql import SparkSession
 from functools import wraps
 
@@ -32,8 +33,18 @@ class DeltabricksTableSpec:
     )
 
   @staticmethod
-  def from_ddl(ddl: str) -> DeltabricksTableSpec:
-    raise NotImplementedError()
+  def from_ddl_info(ddl_info: Dict[str,Any]) -> DeltabricksTableSpec:
+
+      columns = []
+      for c_name, c_type, c_comment in ddl_info['columns']:
+        columns.append({c_name:{"type":c_type, "comment": c_comment}})
+
+      return DeltabricksTableSpec(
+        table_name=ddl_info['table']
+        , database_name=ddl_info['database']
+        , columns=columns
+        , using=ddl_info['provider']
+      )
 
   def to_create_table_ddl(self):
     raise NotImplementedError()
@@ -57,6 +68,27 @@ class DeltabricksBackend:
   def get_ddl(self, database_name: str, table_name: str) -> str:
     
     return self.sql(f"SHOW TABLE EXTENDED IN {database_name} LIKE '{table_name}'")
+
+
+  def _get_table_struct_info(self, db_name: str, table_name: str) -> Dict[str, Any]:
+    struct_info = self.spark.sql(f"SHOW TABLE EXTENDED in {db_name} LIKE '{table_name}'").limit(1).collect()[0].information
+    tokens = filter(lambda x: not is_none_or_empty_string(x), struct_info.split('\n'))
+    tokens = [str.lower(t) for t in tokens]
+    return dict([x.strip() for x in t.split(':', 1)] for t in tokens)
+
+  def _get_table_col_info(self, db_name: str, table_name: str) -> Generator[Tuple[str, str,str]]:
+    col_info = self.spark.sql(f"DESCRIBE TABLE {db_name}.{table_name}").where("data_type != null").collect()
+
+    for row in col_info:
+      yield row.col_name, row.data_type, row.comment
+
+  def get_ddl_info(self, database_name: str, table_name: str) -> Dict[str, Any]:
+    
+    ddl_info = self._get_table_col_info(database_name,table_name)
+    ddl_info['columns'] = list(self._get_table_col_info(database_name, table_name))
+
+    return ddl_info
+
 
 
 def action(m):
@@ -90,7 +122,7 @@ class DeltabricksDancer():
     if not self.backend.table_exists(vc_table_spec.database_name,vc_table_spec.table_name):
       self.backend.sql(vc_table_spec.to_create_table_ddl())
 
-    backend_table_spec = DeltabricksTableSpec.from_ddl(self.backend.get_ddl(vc_table_spec.database_name,vc_table_spec.table_name))
+    backend_table_spec = DeltabricksTableSpec.from_ddl_info(self.backend.get_ddl(vc_table_spec.database_name,vc_table_spec.table_name))
 
     if not vc_table_spec.is_same(backend_table_spec):
       
