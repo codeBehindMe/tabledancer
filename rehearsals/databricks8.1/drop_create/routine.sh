b31905885616b9d5071e7bda9ff7675b2713dd0e
codeBehindMe <aaron.tillekeratne@gmail.com>
2021-08-23 10:45:56 +0000

    Added extra quote

--- rehearsals/databricks8.1/drop_create/routine.sh
+++ rehearsals/databricks8.1/drop_create/routine.sh
@@ -5,7 +5,7 @@ set -exo pipefail
 echo "Installing requirements"
 python setup.py install --user --style databricks8.1
 
-echo {\"host\":\"$DATABRICKS_ADDRESS\", \"token\":\"$DATABRICKS_API_TOKEN\", \"cluster_id\":\"$DATABRICKS_CLUSTER_ID\",\"org_id\":\"0\",\"port\":\"15001\"} > $HOME/.databricks-connect
+echo {\"host\":\""$DATABRICKS_ADDRESS"\", \"token\":\"$DATABRICKS_API_TOKEN\", \"cluster_id\":\"$DATABRICKS_CLUSTER_ID\",\"org_id\":\"0\",\"port\":\"15001\"} > $HOME/.databricks-connect
 
 echo "Preparing database"
 python rehearsals/databricks8.1/drop_create/prepare.py
