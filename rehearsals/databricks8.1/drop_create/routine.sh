9bfa1326f965c9db1729ee531632941cae58f60c
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2021-08-23 04:09:20 +0000

    Added db configure step

--- rehearsals/databricks8.1/drop_create/routine.sh
+++ rehearsals/databricks8.1/drop_create/routine.sh
@@ -5,6 +5,8 @@ set -euxo pipefail
 echo "Installing requirements"
 python setup.py install --user --style databricks8.1
 
+databricks-connect configure -y
+
 echo "Preparing database"
 python rehearsals/databricks8.1/drop_create/prepare.py
 
